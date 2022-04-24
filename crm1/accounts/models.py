28fa9116a9b474a25a68435840c6f95ebcaf96e5
duylunlxag <duylunlxag@gmail.com>
2022-04-24 20:27:18 +0700

    Crash Course Tutorials pt 16

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Customer(models.Model):
+	user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, null=True)
 	phone = models.CharField(max_length=200, null=True)
 	email = models.CharField(max_length=200, null=True)
@@ -10,6 +12,7 @@ class Customer(models.Model):
 
 	def __str__(self):
 		return self.name
+
 
 class Tag(models.Model):
 	name = models.CharField(max_length=200, null=True)
@@ -19,9 +22,9 @@ class Tag(models.Model):
 
 class Product(models.Model):
 	CATEGORY = (
-		('Indoor', 'Indoor'),
-		('Out Door', 'Out Door'),
-		)
+			('Indoor', 'Indoor'),
+			('Out Door', 'Out Door'),
+			) 
 
 	name = models.CharField(max_length=200, null=True)
 	price = models.FloatField(null=True)
@@ -35,16 +38,16 @@ class Product(models.Model):
 
 class Order(models.Model):
 	STATUS = (
-		('Pending', 'Pending'),
-		('Out for delivery', 'Out for delivery'),
-		('Delivered', 'Delivered'),
-		)
+			('Pending', 'Pending'),
+			('Out for delivery', 'Out for delivery'),
+			('Delivered', 'Delivered'),
+			)
 
-	customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
-	product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+	customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+	product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	status = models.CharField(max_length=200, null=True, choices=STATUS)
 	note = models.CharField(max_length=1000, null=True)
-	
+
 	def __str__(self):
 		return self.product.name
